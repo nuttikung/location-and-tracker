aa4ddceaeb90a491779017c61fc9d4460a5bcf73
Mahi-Teja <mahineeli899@gmail.com>
2024-03-05 17:19:59 +0530

    replicated signup from login

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,75 +1,113 @@
-export const SignUp = () => {
+import { useReducer } from 'react'
+import {
+  type SignupForm,
+  SignupFormField,
+  SignupAction,
+  SignupDispatch,
+} from '@/types'
+import { Link } from 'react-router-dom'
+
+// TODO: extract to new file ?
+const INITIAL_Signup_FORM_STATE: SignupForm = {
+  username: '',
+  email: '',
+  password: '',
+}
+
+// TODO: extract this to state file ?
+function reducer(state: SignupForm, action: SignupDispatch) {
+  const { type, payload } = action
+  switch (type) {
+    case SignupAction.CHANGE_VALUE:
+      return {
+        ...state,
+        [action.field]: payload,
+      }
+    default:
+      return state
+  }
+}
+
+export const Signup = () => {
+  const [formState, dispatch] = useReducer(reducer, INITIAL_Signup_FORM_STATE)
+
+  const handleInputChange =
+    (field: SignupFormField) => (event: React.FormEvent<HTMLInputElement>) => {
+      dispatch({
+        type: SignupAction.CHANGE_VALUE,
+        payload: event.currentTarget.value,
+        field,
+      })
+    }
+
+  const handleFormSubmit = (event: React.SyntheticEvent) => {
+    event.preventDefault()
+    // TODO: validate form don't send to BE if no user or password provide.
+    if (formState.password !== '' && formState.username !== '') {
+      // submit to backend.
+      console.log('🚀 ~ handleFormSubmit ~ formState:', formState)
+    }
+  }
+
   return (
-    <main className="form-bg text-slate-300 h-screen flex justify-center ">
-      <div className=" flex flex-col justify-center p-2 ">
-        <div className="text-3xl p-2 font-bold">Sign Up</div>
+    <main className="flex h-screen justify-center bg-slate-800 text-slate-300">
+      <div className=" flex flex-col justify-center p-2">
+        <div className="p-2 text-center text-3xl font-bold">Signup</div>
         <div className="m-3">
-          <form action="" className="flex flex-col  flex-wrap">
-            {/* form */}
-            <div className="flex flex-col md:flex-row">
-              <input
-                type="text"
-                name="first-name"
-                id="first-name"
-                placeholder="First Name"
-                className="m-1 p-2 form-input-text text-xl outline-none font-bold rounded-lg "
-                required
-              />
-              <input
-                type="text"
-                name="last-name"
-                id="last-name"
-                placeholder="Last Name"
-                className="m-1 p-2 form-input-text text-xl outline-none font-bold rounded-lg "
-                required
-              />
-            </div>
+          <form
+            onSubmit={handleFormSubmit}
+            noValidate
+            className="flex flex-col flex-wrap"
+          >
             <input
               type="text"
               name="username"
               id="username"
               placeholder="Username"
-              className="m-1 p-2 form-input-text text-xl outline-none font-bold rounded-lg"
-              required
+              value={formState.username}
+              onChange={handleInputChange('username')}
+              autoComplete="username"
+              className="m-1 rounded-lg p-2 text-xl font-bold text-slate-700 outline-none"
+            />
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="email"
+              value={formState.email}
+              onChange={handleInputChange('email')}
+              autoComplete="email"
+              className="m-1 rounded-lg p-2 text-xl font-bold text-slate-700 outline-none"
             />
             <input
               type="password"
               name="password"
               id="password"
+              value={formState.password}
+              onChange={handleInputChange('password')}
+              autoComplete="current-password"
               placeholder="password"
-              className="m-1 p-2 form-input-text text-xl outline-none font-bold rounded-lg"
-              required
+              className="m-1 rounded-lg p-2 text-xl font-bold text-slate-700 outline-none"
             />
-            <input
-              type="password"
-              name="confirm-password"
-              id="cofirm-password"
-              placeholder="confirm password"
-              className="m-1 p-2 form-input-text text-xl outline-none font-bold rounded-lg"
-              required
-            />
-            <div className="self-start p-2 m-1">
-              <input
-                type="checkbox"
-                name="accept-t&c"
-                id="accept-t&c"
-                className="m-1 p-2 form-input-text text-xl outline-none font-bold rounded-lg"
-                required
-              />
-              <label htmlFor="accept-t&c">Accepting our T&C</label>
-            </div>
-            <button type="submit" className="form-button m-2 p-2 text-xl ">
-              Sign up
+            <button type="submit" className="m-2 bg-slate-500 p-2 text-xl">
+              Log in
             </button>
           </form>
         </div>
-        <div className="">
-          {/* <div className="p-1 form-other-text"><a className="hover:text-slate-400 hover:underline " href="">forgot Password?</a></div> */}
-          <div className="p-1 form-other-text">
-            Already have an account?
-            <a className="hover:text-slate-400 hover:underline p-1" href="/Login">
-               Log In!
-            </a>
+        <div>
+          <div className="p-1 text-center text-slate-500">
+            <Link to="/" className=" hover:text-slate-400 hover:underline">
+              Forgot Password?
+            </Link>
+          </div>
+          <div className="p-1 text-center text-slate-500">
+            Already a user?{' '}
+            <Link
+              to="/login"
+              className=" hover:text-slate-400 hover:underline"
+            >
+              Log In!
+            </Link>
           </div>
         </div>
       </div>
