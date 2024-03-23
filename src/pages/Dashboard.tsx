4f0c55241d814f64489edad70481aa4e70647207
Mahi-Teja <mahineeli899@gmail.com>
2024-03-23 13:08:51 +0530

    changed a ti Link

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,6 @@
 export const Dashboard = () => {
 	return <main>
-		<button className="p-1 m-3 bg-blue-700"  ><a href="/login"  className="p-3">Login</a></button>
-		<button className="p-1 m-3 bg-orange-700"><a href="/signup" className="p-3">Signup</a></button>
+		<button className="p-1 m-3 bg-blue-700"  ><Link to="/login"  className="p-3">Login</Link></button>
+		<button className="p-1 m-3 bg-orange-700"><Link to="/signup" className="p-3">Signup</Link></button>
 	</main>;
 };
